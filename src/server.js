d4133cf172fd51ee19342caa58298b7bf5dd0ea2
Felix Heras <heras.felix@gmail.com>
2018-06-21 11:22:49 +0200

    Fix: Clean code

--- src/server.js
+++ src/server.js
@@ -3,9 +3,6 @@ const translate = require('./translator')
 const text = "quiero trabajar con los mejores"
 const targetLang = 'en'
 
-//console.log(translate);
-
 translate.translateText(text, targetLang)
   .then((x)=>console.log(x))
   .catch((error)=>console.error(error))
-bootsnipps
